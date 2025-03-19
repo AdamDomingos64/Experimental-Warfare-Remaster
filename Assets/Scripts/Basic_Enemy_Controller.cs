38d19cd1b78ca1e35a197a8721bf5d1f841d8ac8
AdamDomingos64 <160789760+AdamDomingos64@users.noreply.github.com>
2025-03-19 18:24:36 -0400

    enemy turn

--- Assets/Scripts/Basic_Enemy_Controller.cs
+++ Assets/Scripts/Basic_Enemy_Controller.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,13 +8,15 @@ public class Basic_Enemy_Controller : MonoBehaviour
 {
 
     public float speedOr;
-    public GameObject Target;
+    public Transform Target;
     public GameObject Ammunition;
     public int Health;
     public float attackRange;
     public float cooldown;
     private float cooling;
     private float speed;
+    public Vector2 targetPosition;
+
 
     // Start is called before the first frame update
     void Start()
@@ -26,7 +29,14 @@ public class Basic_Enemy_Controller : MonoBehaviour
     void Update()
     {
 
+
+
+       // targetPosition = Target.transform.position;
+
         cooling -= Time.deltaTime;
+
+
+
         if (cooling <= 0)
         {
             cooling = cooldown;
@@ -34,15 +44,27 @@ public class Basic_Enemy_Controller : MonoBehaviour
 
         if (Target != null)
         {
+           
             transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, speed * Time.deltaTime);
+
+             Vector3 delta = Target.transform.position - transform.position;
+
+            float zAngle = Mathf.Atan2(delta.y, delta.x);
+            zAngle = zAngle * Mathf.Rad2Deg - 90f;
+
+            Vector3 newRotation = Vector3.zero;
+            newRotation.z = zAngle;
+            transform.rotation = Quaternion.Euler(newRotation);
         }
 
         if (Vector2.Distance(transform.position, Target.transform.position) <= attackRange)
         {
+            Debug.Log("stop");
              speed = 0;
         }
         else if(Vector2.Distance(transform.position, Target.transform.position) > attackRange)
         {
+            Debug.Log("find");
             speed = speedOr;
         }
 
