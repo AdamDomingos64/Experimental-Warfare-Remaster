e692c0ba4168e0dd7f21e48c7c51e297c1e7f3af
AdamDomingos64 <adamdomingos17@gmail.com>
2025-03-21 00:17:38 -0400

    artillery

---
+++ Assets/Scripts/Artillery_Controller.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Artillery_Controller : MonoBehaviour
+{
+
+    public GameObject Target;
+   
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        
+
+        // Vector3 Look = transform.InverseTransformPoint(Target.transform.position);
+        // float Angle = Mathf.Atan2(Look.y, Look.x) * Mathf.Rad2Deg - 90;
+
+        // transform.Rotate(0,0,Angle);
+
+
+        // var dir = Target.transform.position - transform.position;
+        // var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        // transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+    }
+}
