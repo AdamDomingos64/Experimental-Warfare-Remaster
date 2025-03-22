fc4e6446965ced321085bb75c97bda8654080b15
AdamDomingos64 <adamdomingos17@gmail.com>
2025-03-22 16:45:23 -0400

    idk about artillery movement

--- Assets/Scripts/Artillery_Controller.cs
+++ Assets/Scripts/Artillery_Controller.cs
@@ -6,8 +6,8 @@ using static UnityEngine.GraphicsBuffer;
 public class Artillery_Controller : MonoBehaviour
 {
 
-    public GameObject Target;
-   
+    public Transform Target;
+   public float speed = 1.0f;
     
 
     // Start is called before the first frame update
@@ -20,7 +20,25 @@ public class Artillery_Controller : MonoBehaviour
     void Update()
     {
 
+        if (Target != null)
+        {
 
+            transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, speed * Time.deltaTime);
+
+            Vector3 delta = Target.transform.position - transform.position;
+
+            float zAngle = Mathf.Atan2(delta.y, delta.x);
+            zAngle = zAngle * Mathf.Rad2Deg - 90f;
+
+            Vector3 newRotation = Vector3.zero;
+            newRotation.z = zAngle;
+            transform.rotation = Quaternion.Euler(newRotation);
+
+            //transform.LookAt(Target, transform.up);
+
+            //transform.LookAt(Target.position, Vector3.up);
+
+        }
 
         
 
