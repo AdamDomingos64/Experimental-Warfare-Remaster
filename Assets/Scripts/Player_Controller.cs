2543dfc4276bff4b47f847540f67f92df9c60be2
AdamDomingos64 <160789760+AdamDomingos64@users.noreply.github.com>
2025-03-25 13:51:17 -0400

    new movement+ animations

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -6,6 +6,7 @@ using UnityEngine.UIElements;
 public class Player_Controller : MonoBehaviour
 {
     public float Speed;
+    public float lookSpeed;
     public Rigidbody2D Rb2d;
     private Vector2  moveInput;
     private SpriteRenderer spriteRenderer;
@@ -20,26 +21,32 @@ public class Player_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
 
+        transform.position += transform.up * Time.deltaTime * Speed;
+
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        /*        moveInput.y = Input.GetAxisRaw("Vertical");
+
+
+
+                moveInput.Normalize();*/
         
 
-        moveInput.Normalize();
-
-        Rb2d.velocity = moveInput * Speed;
+        //Rb2d.velocity = moveInput * Speed;
 
         if (moveInput.x > 0) 
         {
-            transform.rotation = Quaternion.Euler(0, 0, -90);
+            //transform.rotation = Quaternion.Euler(0, 0, -90 * Time.deltaTime);
+            transform.Rotate(0, 0, -10 * Time.deltaTime * lookSpeed);
         }
 
          else if (moveInput.x < 0)
         {
-            transform.rotation = Quaternion.Euler(0, 0, 90);
+            //transform.rotation = Quaternion.Euler(0, 0, 90);
+            transform.Rotate(0, 0, 10 * Time.deltaTime * lookSpeed);
         }
 
-             else if (moveInput.y > 0)
+        /*     else if (moveInput.y > 0)
         {
             transform.rotation = Quaternion.Euler(0, 0, 0);
         }
@@ -48,7 +55,7 @@ public class Player_Controller : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(0, 0, 180);
         }
-
+*/
         if (Input.GetKeyDown("space"))
         {
             Instantiate(Ammunition, transform.position, transform.rotation);
